747b305fe574bc795ca13ae9db2132336258a600
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-04-01 21:48:25 +0530

    Tested Mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
+// Global Imports
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+
+// Project imports
+const userModel = require("./models/User");
+
+// Loading  the Environment variables
 process.loadEnvFile(".env");
+
+// Connecting to the database
+mongoose.connect(process.env.DB_URL);
 
 const port = process.env.PORT || 4000;
 
@@ -10,4 +20,5 @@ app.use(express.json());
 // Static Files
 app.use("/images", express.static("./static/images"));
 
+// Listening to requests
 app.listen(port, () => console.log(`Server live on http://localhost:${port}`));
