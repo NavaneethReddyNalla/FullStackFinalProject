d0f9f379d653f8f53db0ba510cac26bfdebddf95
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-04-17 23:11:27 +0530

    Created the header

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import "./Header.css";
 import React from "react";
 
@@ -5,6 +6,19 @@ function Header() {
   return (
     <header>
       <span className="title">Introvert Matrimony</span>
+      <nav title="navigation-bar">
+        <ul className="nav-buttons">
+          <NavLink className="nav-button" to="/">
+            Home
+          </NavLink>
+          <NavLink className="nav-button" to="/login">
+            Login
+          </NavLink>
+          <NavLink className="nav-button" to="/signup">
+            Sign Up
+          </NavLink>
+        </ul>
+      </nav>
     </header>
   );
 }
