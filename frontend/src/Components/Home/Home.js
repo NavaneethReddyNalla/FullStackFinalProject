0647593d3dc5df64a68d58997b221ad759240748
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-04-20 15:35:59 +0530

    Added some text to the home page

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -2,7 +2,23 @@ import "./Home.css";
 import React from "react";
 
 function Home() {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <p>
+        Welcome to Our Matrimony, where we believe in helping you find not just
+        a partner, but the right partner for a lifetime of happiness. Our
+        platform is designed with a deep understanding of your needs and
+        aspirations. We use advanced algorithms and comprehensive profiling to
+        match you with potential life partners who share your values, interests,
+        and life goals. Our aim is to make your journey towards finding your
+        perfect match not just simpler, but also enjoyable. With Our Matrimony,
+        you're not just getting a service, you're gaining a partner in your
+        journey of love and commitment. Let us help you find the one who
+        completes you, because we understand that love isn't about finding the
+        perfect person, but about finding the person who makes you feel perfect.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
