b9f04ea8a8d92f23023a11d352f1c8a4e1b72834
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-04-21 16:41:24 +0530

    Configured Redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,15 @@ import "./index.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { store } from "./Redux/store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
