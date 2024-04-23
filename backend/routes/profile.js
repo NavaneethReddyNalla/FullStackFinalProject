066b8240f6e7a2199e83520ef1d1eee405ae0d7e
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-04-24 00:19:43 +0530

    Encapsulated the async error handler of photos upload in express async handler

--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -9,36 +9,40 @@ const profileRouter = express.Router();
 
 // Complete Profile by updating
 profileRouter.put("/update/:profileId", verifyToken, (req, res) => {
-  photosUpload(req, res, async (err) => {
-    if (err) {
-      console.log(err);
-      return res.send({
-        message: "Error Occured while uploading the images",
-        status: 11,
-      });
-    } else {
-      // console.log(req.files);
-      const profileId = req.params.profileId;
-      const profile = JSON.parse(req.body.user);
-      profile.photos = req.files.map((file) => file.filename);
-      // console.log(profile);
+  photosUpload(
+    req,
+    res,
+    expressAsyncHandler(async (err) => {
+      if (err) {
+        console.log(err);
+        return res.send({
+          message: "Error Occured while uploading the images",
+          status: 11,
+        });
+      } else {
+        // console.log(req.files);
+        const profileId = req.params.profileId;
+        const profile = JSON.parse(req.body.user);
+        profile.photos = req.files.map((file) => file.filename);
+        // console.log(profile);
 
-      // const profile = req.body;
+        // const profile = req.body;
 
-      const dbProfile = await ProfileModel.findByIdAndUpdate(
-        profileId,
-        { ...profile, profileComplete: true },
-        {
-          new: true,
-        }
-      );
+        const dbProfile = await ProfileModel.findByIdAndUpdate(
+          profileId,
+          { ...profile, profileComplete: true },
+          {
+            new: true,
+          }
+        );
 
-      delete dbProfile.__v;
-      delete dbProfile.user;
+        delete dbProfile.__v;
+        delete dbProfile.user;
 
-      res.send({ message: "Profile Updated", status: 7, profile: dbProfile });
-    }
-  });
+        res.send({ message: "Profile Updated", status: 7, profile: dbProfile });
+      }
+    })
+  );
 });
 
 module.exports = profileRouter;
