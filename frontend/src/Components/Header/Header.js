62141784f5f70d157879738e01d36f30bd443e7a
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-04-26 23:11:53 +0530

    Changed the header

--- frontend/src/Components/Header/Header.js
+++ frontend/src/Components/Header/Header.js
@@ -1,5 +1,6 @@
-import { NavLink, useNavigate } from "react-router-dom";
 import "./Header.css";
+import Logo from "../../Assets/introvert_matrimony_logo.png";
+import { NavLink, useNavigate } from "react-router-dom";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { resetState } from "../../Redux/Slices/userLoginSlice";
@@ -18,7 +19,15 @@ function Header() {
 
   return (
     <header>
-      <span className="title">Introvert Matrimony</span>
+      <div>
+        <img
+          className="me-3 logo"
+          src={Logo}
+          alt="LOGO"
+          style={{ width: "60px" }}
+        />
+        <span className="text-start title">Introvert Matrimony</span>
+      </div>
       <nav title="navigation-bar">
         <ul className="nav-buttons">
           <NavLink className="nav-button" to="/">
@@ -35,15 +44,48 @@ function Header() {
             </>
           ) : (
             <>
-              <button
+              {/* <button
                 className="profile-button"
                 onClick={() => navigate(`${currentUser.username}`)}
               >
                 <ProfilePic imageSrc={currentUser.photo} dimension="50px" />
-              </button>
-              <button className="btn btn-danger" onClick={logout}>
-                Sign Out
-              </button>
+              </button> */}
+              <div class="dropdown">
+                <button
+                  class="btn dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <ProfilePic imageSrc={currentUser.photo} dimension="50px" />
+                </button>
+                <ul class="dropdown-menu">
+                  <li>
+                    <button
+                      className="dropdown-item btn"
+                      onClick={() => navigate(`${currentUser.username}`)}
+                    >
+                      Profile
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="dropdown-item btn"
+                      onClick={() => navigate(`${currentUser.username}`)}
+                    >
+                      Matches
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="btn btn-danger dropdown-item"
+                      onClick={logout}
+                    >
+                      Sign Out
+                    </button>
+                  </li>
+                </ul>
+              </div>
             </>
           )}
         </ul>
