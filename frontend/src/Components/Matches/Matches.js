808537c23b30448e100448c4a5f1ac7f6bf1d628
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-05-03 01:06:46 +0530

    Implemented Search bar

--- frontend/src/Components/Matches/Matches.js
+++ frontend/src/Components/Matches/Matches.js
@@ -5,10 +5,12 @@ import { useSelector } from "react-redux";
 import getAxiosWithToken from "../util";
 import ProfilePic from "../ProfilePic/ProfilePic";
 import { useNavigate } from "react-router-dom";
+import SearchBar from "../SearchBar/SearchBar";
 
 function Matches() {
   const [matches, setMatches] = useState([]);
   const [err, setErr] = useState("");
+  const [query, setQuery] = useState("");
   const { currentUser } = useSelector((state) => state.userLogin);
   const navigate = useNavigate();
 
@@ -34,50 +36,56 @@ function Matches() {
     <div className="mt-5">
       {err !== "" && <p className="lead fs-6 text-danger">{err}</p>}
       <div className="container-fluid">
+        <SearchBar setQuery={setQuery} />
         <div className="row row-cols-4">
           {matches.length > 0 ? (
             <>
-              {matches.map((match) => {
-                return (
-                  <div className="col" key={match._id}>
-                    <div className="card w-100 user-card">
-                      <div className="card-body">
-                        <ProfilePic
-                          imageSrc={match.photo}
-                          dimension={"100px"}
-                        />
-                        <h4 className="card-title">{match.name}</h4>
-                        <h6 className="card-sub-title">
-                          Job: {match.profile.job}
-                        </h6>
-                        <p className="card-text">
-                          Education: {match.profile.highestLevelOfEducation}
-                        </p>
-                        <p className="card-text">Age: {match.profile.age}</p>
-                        <p className="card-text">
-                          State: {match.profile.residenceState}
-                        </p>
-                        <p className="card-text">
-                          Salary Range: {match.profile.salaryRange}
-                        </p>
-                        <p className="card-text">
-                          Date of Birth: {match.profile.dob}
-                        </p>
-                        <p className="card-text">
-                          Horoscope: {match.profile.horoscope}
-                        </p>
+              {matches
+                .filter(
+                  (match) =>
+                    match.username.includes(query) || match.name.includes(query)
+                )
+                .map((match) => {
+                  return (
+                    <div className="col" key={match._id}>
+                      <div className="card w-100 user-card">
+                        <div className="card-body">
+                          <ProfilePic
+                            imageSrc={match.photo}
+                            dimension={"100px"}
+                          />
+                          <h4 className="card-title">{match.name}</h4>
+                          <h6 className="card-sub-title">
+                            Job: {match.profile.job}
+                          </h6>
+                          <p className="card-text">
+                            Education: {match.profile.highestLevelOfEducation}
+                          </p>
+                          <p className="card-text">Age: {match.profile.age}</p>
+                          <p className="card-text">
+                            State: {match.profile.residenceState}
+                          </p>
+                          <p className="card-text">
+                            Salary Range: {match.profile.salaryRange}
+                          </p>
+                          <p className="card-text">
+                            Date of Birth: {match.profile.dob}
+                          </p>
+                          <p className="card-text">
+                            Horoscope: {match.profile.horoscope}
+                          </p>
 
-                        <button
-                          className="btn btn-outline-success"
-                          onClick={() => navigate("match", { state: match })}
-                        >
-                          View
-                        </button>
+                          <button
+                            className="btn btn-outline-success"
+                            onClick={() => navigate("match", { state: match })}
+                          >
+                            View
+                          </button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
             </>
           ) : (
             [1, 2, 3, 4, 5, 6, 7, 8].map((id) => {
---
+++ frontend/src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function SearchBar({ setQuery }) {
+  function handleChange(event) {
+    setQuery(event.target.value);
+  }
+
+  return (
+    <div className="search-bar">
+      <input
+        type="text"
+        className="form-control w-50 mx-auto mb-5"
+        placeholder="Search by username or full name..."
+        onChange={handleChange}
+      />
+    </div>
+  );
+}
+
+export default SearchBar;
