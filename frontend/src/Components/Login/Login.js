4d84b4ab37c78037ff2d9b9534e3465a9842e376
Navaneeth Reddy Nalla <navaneethreddynalla@gmail.com>
2024-05-06 01:55:59 +0530

    Added a Login Image to login form

--- frontend/src/Components/Login/Login.js
+++ frontend/src/Components/Login/Login.js
@@ -4,6 +4,7 @@ import { useForm } from "react-hook-form";
 import { useDispatch, useSelector } from "react-redux";
 import { userLoginThunk } from "../../Redux/Slices/userLoginSlice";
 import { useNavigate } from "react-router-dom";
+import LoginPic from "../../Assets/login.jpg";
 
 function Login() {
   const {
@@ -29,35 +30,51 @@ function Login() {
 
   return (
     <div>
-      <form className="login-form" onSubmit={handleSubmit(handleLogin)}>
-        <input
-          type="text"
-          placeholder="Username"
-          {...register("username", { required: true })}
-          className="form-control"
-        />
-        {errors.username?.type === "required" && (
-          <p className="lead fs-6 text-danger">Username can't be empty</p>
-        )}
+      <div className="login" id="login">
+        <img src={LoginPic} alt="Login" />
+        <form className="login-form" onSubmit={handleSubmit(handleLogin)}>
+          <input
+            type="text"
+            placeholder="Username"
+            {...register("username", { required: true })}
+            className="form-control"
+          />
+          {errors.username?.type === "required" && (
+            <p className="lead fs-6 text-danger">Username can't be empty</p>
+          )}
 
-        <input
-          type="password"
-          placeholder="Password"
-          {...register("password", { required: true })}
-          className="form-control"
-        />
-        {errors.password?.type === "required" && (
-          <p className="lead fs-6 text-danger">Password is required</p>
-        )}
+          <input
+            type="password"
+            placeholder="Password"
+            {...register("password", { required: true })}
+            className="form-control"
+          />
+          {errors.password?.type === "required" && (
+            <p className="lead fs-6 text-danger">Password is required</p>
+          )}
 
-        {errorOccured && (
-          <p className="lead fs-6 text-danger">{errorMessage}</p>
-        )}
+          {errorOccured && (
+            <p className="lead fs-6 text-danger">{errorMessage}</p>
+          )}
 
-        <button type="submit" className="btn btn-primary">
-          Login
-        </button>
-      </form>
+          <p>
+            Don't have an account yet?{" "}
+            <a
+              href="#login"
+              onClick={(event) => {
+                event.preventDefault();
+                navigate("/register");
+              }}
+            >
+              Sign In
+            </a>
+          </p>
+
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
